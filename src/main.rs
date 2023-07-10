9157a736ed173aa589663ec763974d4349ff93cb
Eray Alkış <lolpo12345@outlook.com>
2023-07-10 04:27:59 +0300

    Add error handling

--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,37 @@ fn main() {
             },
 
             (GET) (/up/{image_name: String}) => {
-                let res_text = format!("Hello {}", image_name);
-                Command::new("docker")
+                let cmd = Command::new("docker")
                     .arg("start")
                     .arg(image_name)
-                    .spawn()
-                    .expect("Can't run ur image lolz!");
-                rouille::Response::text(res_text)
+                    .status()
+                    .expect("Can't start server!");
+
+                match cmd.success() {
+                    true => {
+                        rouille::Response::text("success")
+                    }
+                    false => {
+                        rouille::Response::text("error")
+                    }
+                }
             },
 
             (GET) (/down/{image_name: String}) => {
-                let res_text = format!("Hello {}", image_name);
-                Command::new("docker")
+                let cmd = Command::new("docker")
                     .arg("stop")
                     .arg(image_name)
-                    .spawn()
-                    .expect("Can't run ur image lolz!");
-                rouille::Response::text(res_text)
+                    .status()
+                    .expect("Can't stop server!");
+
+                match cmd.success() {
+                    true => {
+                        rouille::Response::text("success")
+                    }
+                    false => {
+                        rouille::Response::text("error")
+                    }
+                }
             },
 
             _ => rouille::Response::empty_404()
