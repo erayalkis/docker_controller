472670b99639304951f793f17f9d551cb3bc3b6e
Eray Alkış <lolpo12345@outlook.com>
2023-07-10 03:50:04 +0300

    Create base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+#[macro_use]
+extern crate rouille;
+
+use std::process::Command;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Now listening on localhost:8000");
+
+    rouille::start_server("localhost:8000", move |req| {
+        router!(req,
+            (GET) (/) => {
+                rouille::Response::text("hello world")
+            },
+
+            (GET) (/up/{image_name: String}) => {
+                let res_text = format!("Hello {}", image_name);
+                Command::new("docker")
+                    .arg("start")
+                    .arg(image_name)
+                    .spawn()
+                    .expect("Can't run ur image lolz!");
+                rouille::Response::text(res_text)
+            },
+
+            (GET) (/down/{image_name: String}) => {
+                let res_text = format!("Hello {}", image_name);
+                Command::new("docker")
+                    .arg("stop")
+                    .arg(image_name)
+                    .spawn()
+                    .expect("Can't run ur image lolz!");
+                rouille::Response::text(res_text)
+            },
+
+            _ => rouille::Response::empty_404()
+        )
+    });
 }
